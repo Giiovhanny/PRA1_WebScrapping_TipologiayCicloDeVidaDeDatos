e9ce7f336024fc831e68a66743709a83934e0302
Giiovhanny <gio-vanny1@hotmail.com>
2021-04-03 15:20:23 -0500

    Changes to scrapping

--- scraping1.py
+++ scraping1.py
@@ -2,12 +2,16 @@ from selenium import webdriver
 import pandas as pd
 
 url = 'https://www.youtube.com/c/HealthyPockets/videos'
+webPage='https://www.youtube.com/c/KikeJav/videos'
 
-driver = webdriver.Chrome()
+PATH=r"C:\Users\gio-v\Documents\UOC\Tipologiayciclodevidadelosdatos\chromedriver.exe"
+
+driver = webdriver.Chrome(PATH)
 driver.get(url)
 
-videos = driver.find_element_by_class_name('style-scope ytd-grid-video-renderer')
-
+videos = driver.find_elements_by_class_name('style-scope ytd-grid-video-renderer')
+#videos = driver.find_elements_by_id('items')
+#videos = driver.find_elements_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[1]/div[1]/div[1]')
 
 video_list = []
 for video in videos:
@@ -19,8 +23,8 @@ for video in videos:
         'views' : views,
         'posted' : when,
     }
-
     video_list.append(vid_item)
+
 
 df = pd.DataFrame(video_list)
 print(df)
